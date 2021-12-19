d72f45ec25967e44fcfb7f7014c454d897c948f1
maruhxn <77433220+lhk6397@users.noreply.github.com>
2021-12-19 21:37:49 +0900

    12/19 결과물
    - Features로 ['Win_Lose_Draw', 'LOTTE_ATK_AVG', 'LOTTE_ATK_OPS', 'LOTTE_PIT_ERA', 'LOTTE_PIT_WHIP', 'LOTTE_DEF_A', 'LOTTE_DEF_E',' Cont_Winning_Rate_20', 'HOME_AWAY']를 제공
    - 129개의 데이터
    - 정확도: 0.4736892736892737
    - DRAW를 예측하지 못함.. -> DRAW를 WIN으로 판단하게 변경하면 정확도 0.7041847041847042까지 올릴 수 있음
    => DRAW 어떻게 처리할지..? + 그래프 그리기

--- lotte_skeleton.py
+++ lotte_skeleton.py
@@ -7,17 +7,17 @@ def load_lotte_data(filename):
     class LOTTEData:
         data = []
         target = []
-        target_names = ['LOSE', 'WIN', 'DRAW']
-        feature_names = ['Win_Lose_Draw', 'HOME_AWAY', 'LOTTE_ATK_AVG', 'LOTTE_ATK_OPS', 'LOTTE_PIT_ERA', 'LOTTE_PIT_WHIP', 'LOTTE_DEF_A', 'LOTTE_DEF_E',' Cont_Winning_Rate_20']
+        target_names = ['LOSE', 'WIN', 'DRAW'] # DRAW를 빼면 정확도 0.473 -> 0.704까지
+        feature_names = ['Win_Lose_Draw', 'LOTTE_ATK_AVG', 'LOTTE_ATK_OPS', 'LOTTE_PIT_ERA', 'LOTTE_PIT_WHIP', 'LOTTE_DEF_A', 'LOTTE_DEF_E',' Cont_Winning_Rate_20', 'HOME_AWAY']
     lotte = LOTTEData()
     try:
         with open(filename, 'r') as f:
             for line in f:
-                values = [word for word in line.split(',')]
+                values = [word for word in line.split(', ')]
                 lotte.target.append(0 if values[0] == 'L' else (1 if values[0] == 'W' else 2))
-                lotte.data.append(0 if values[1] == 'H' else 1)
-                value = [float(i) for i in values[2:]]
+                value = [float(i) for i in values[1:8]]
                 lotte.data.append(value)
+                value.append(0 if values[8] == 'H' else 1)
     except Exception as ex:
         print(f'Cannot run the program. (message: {ex})')
     lotte.data = np.array(lotte.data)
@@ -33,4 +33,10 @@ if __name__ == '__main__':
     # Test the model
     predict = model.predict(lotte.data)
     n_correct = sum(predict == lotte.target)
-    accuracy = metrics.balanced_accuracy_score(lotte.target, predict)                      
+    accuracy = metrics.balanced_accuracy_score(lotte.target, predict)   
+    
+    print(predict)
+    print(lotte.target)
+    print(accuracy)                   
+
+# matplotlib 이용해서 그래프 그리기
